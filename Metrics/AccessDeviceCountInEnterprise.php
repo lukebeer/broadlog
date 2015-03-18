033a438d7b3ad855dbb1316ec1663d48e3ad962c
LukeBeer <eat.lemons@gmail.com>
2015-03-18 22:25:07 +0000

    Poor mans cronjob wrapper

---
+++ Metrics/AccessDeviceCountInEnterprise.php
@@ -0,0 +1,24 @@
+<?php
+namespace Broadlog\Metrics;
+require_once '../Config.php';
+require_once BROADWORKS_OCIP_PATH  . '/common.php';
+
+use Broadlog\Crawlers\ServiceProviderCrawler;
+use Broadlog\Crawlers\SystemCrawler;
+use Broadlog\Controllers\MetricController;
+use Broadworks_OCIP\CoreFactory;
+
+
+$client = CoreFactory::getTCPClient(OCIP_HOST);
+$client->login(OCIP_USER, OCIP_PASS);
+
+$controller = new MetricController();
+$systemCrawler = new SystemCrawler($client);
+$serviceProviderCrawler = new ServiceProviderCrawler($client);
+
+foreach ($systemCrawler->getServiceProviderList() as $sp) {
+    $controller->setMetric('broadworks.'.OCIP_NAME.".enterprises.{$sp}.devices");
+    $count = count($serviceProviderCrawler->getAccessDeviceList($sp));
+    $controller->send($count);
+    echo "Devices: $sp $count \n";
+}
---
+++ run.php
@@ -0,0 +1,9 @@
+<?php
+$file = ((isset($argv[1]) && (file_exists($argv[1])))) ? $argv[1] : 'Config.php';
+
+if (file_exists($file)) {
+    chdir(__DIR__.'/Metrics');
+    system('php AdminCountInEnterprise.php');
+    system('php UserCountInEnterprise.php');
+    system('php AccessDeviceCountInEnterprise.php');
+}
